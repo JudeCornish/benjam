b7b53c0429eefdd8b4384ad608407d6de61c0abf
JudeCornish <50799584+JudeCornish@users.noreply.github.com>
2020-05-15 14:22:07 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+$('.wall').jaliswall({
+    item: '.wall-item',
+    columnClass: '.wall-column'
+});
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
